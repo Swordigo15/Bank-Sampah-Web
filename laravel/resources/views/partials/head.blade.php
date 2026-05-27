bbd38248c481bb312879bd50374c418795855809
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2026-05-27 09:20:08 +0700

    Add Select2

--- laravel/resources/views/partials/head.blade.php
+++ laravel/resources/views/partials/head.blade.php
@@ -16,5 +16,7 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet" />
 </head>
--- laravel/resources/views/partials/scripts.blade.php
+++ laravel/resources/views/partials/scripts.blade.php
@@ -14,3 +14,5 @@
 <!-- Page level custom scripts -->
 <script src="js/demo/chart-area-demo.js"></script>
 <script src="js/demo/chart-pie-demo.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
