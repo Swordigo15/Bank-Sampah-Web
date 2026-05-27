4f6b9af6d993976133327e2dab30f38dd60339da
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2026-05-27 14:54:09 +0700

    Add sweetalert

--- laravel/resources/views/partials/scripts.blade.php
+++ laravel/resources/views/partials/scripts.blade.php
@@ -14,3 +14,57 @@
 <script src="{{ URL::asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 {{-- <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@if (session('success'))
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            Swal.fire({
+                title: 'Sukses!',
+                text: '{{ session('success') }}',
+                icon: 'success',
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'btn btn-primary w-xs'
+                },
+                buttonsStyling: false,
+                showCloseButton: true
+            });
+        });
+    </script>
+@endif
+
+@if (session('error'))
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            Swal.fire({
+                title: 'Warning!',
+                text: 'Terjadi kesalahan pada sistem.',
+                icon: 'error',
+                customClass: {
+                    confirmButton: 'btn btn-primary w-xs mt-2',
+                },
+                buttonsStyling: false,
+                footer: '{{ session('error') }}',
+                showCloseButton: true
+            });
+        });
+    </script>
+@endif
+
+@if ($errors->any())
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            Swal.fire({
+                title: 'Validasi Gagal!',
+                html: `{!! implode('<br>', $errors->all()) !!}`,
+                icon: 'error',
+                customClass: {
+                    confirmButton: 'btn btn-primary w-xs mt-2',
+                },
+                buttonsStyling: false,
+                showCloseButton: true
+            });
+        });
+    </script>
+@endif
