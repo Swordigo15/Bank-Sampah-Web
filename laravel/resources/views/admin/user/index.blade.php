a8451c19932dc4d76cdce42d17232fddf15edbb9
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2026-05-27 18:22:48 +0700

    update index template

--- laravel/resources/views/admin/user/index.blade.php
+++ laravel/resources/views/admin/user/index.blade.php
@@ -18,7 +18,7 @@
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             {{-- <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6> --}}
-            <a class="btn btn-success" href="{{ route('admin.user.create') }}">Tambah User</a>
+            <a class="btn btn-success" href="{{ route($route . '.create') }}">Tambah {{ $nama }}</a>
         </div>
         <div class="card-body">
             <div class="table-responsive">
