723ebb638ed9b08c200b3741374f17f4a6114663
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2026-05-27 10:13:50 +0700

    Update head and script partial

--- laravel/resources/views/partials/scripts.blade.php
+++ laravel/resources/views/partials/scripts.blade.php
@@ -1,18 +1,18 @@
 <!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="{{ URL::asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="js/sb-admin-2.min.js"></script>
+<script src="{{ URL::asset('js/sb-admin-2.min.js') }}"></script>
 
 <!-- Page level plugins -->
-<script src="vendor/chart.js/Chart.min.js"></script>
+<script src="{{ URL::asset('vendor/chart.js/Chart.min.js') }}"></script>
 
 <!-- Page level custom scripts -->
-<script src="js/demo/chart-area-demo.js"></script>
-<script src="js/demo/chart-pie-demo.js"></script>
-{{-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> --}}
+<script src="{{ URL::asset('js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ URL::asset('js/demo/chart-pie-demo.js') }}"></script>
+{{-- <script src="{{ URL::asset('vendor/jquery/jquery.min.js') }}"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
